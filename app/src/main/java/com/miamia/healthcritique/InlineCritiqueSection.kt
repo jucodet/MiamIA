ffea6cc604b799474d41de079f43284a1c483b0e
Julien Codet <julien.codet@niji.fr>
2026-06-28 18:21:55 +0200

    Rename HealthCritiqueResultScreen to InlineCritiqueSection

--- app/src/main/java/com/miamia/healthcritique/InlineCritiqueSection.kt
+++ app/src/main/java/com/miamia/healthcritique/InlineCritiqueSection.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
